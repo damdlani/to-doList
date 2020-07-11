43b1e76eca75f94d8a59360d3d260dd41d2c70b7
Krzysztof Kwiecinski <kkwiecinski97@gmail.com>
2020-07-11 17:36:22 +0200

    modified css, added trial functions

--- JS/script.js
+++ JS/script.js
@@ -8,20 +8,24 @@
             content: "test",
             done: true,
         },
-        task = {
-            content: "test",
-            done: true,
-        },
-        task = {
-            content: "test",
-            done: true,
-        },
     ]
     const render = (tasks) => {
         for (task of tasks) {
-            tasksListElement.innerHTML += `<li class="todo__task"><button class="todo__button todo__button--check js-checkButton"><i class="fas fa-check todo__checkMark"></i></button><span class="todo__span">${task.content}</span><button class="todo__button todo__button--remove js-removeButton"><i class="fas fa-trash-alt"></i></button></li>`
+            tasksListElement.innerHTML += `<li class="todo__task"><button class="todo__button todo__button--check js-checkButton"><i class="fas fa-check todo__checkMark"></i></button><span class="todo__span js-taskSpan">${task.content}</span><button class="todo__button todo__button--remove js-removeButton"><i class="fas fa-trash-alt"></i></button></li>`
         };
     }
+    const crossOut = () => {
+        const taskSpan = document.querySelector(".js-taskSpan");
+        taskSpan.classList.toggle("todo__span--done");
+    };
+    const checkMark = () => {
+        checkButton.classList.toggle("todo__checkMark")
+    };    
+    const onClickCheck = () => {
+        const checkButton = document.querySelectorAll(".js-checkButton");
+        console.log(checkButton);
+    }
+    onClickCheck();
     const onFormSubmit = () => {
         if (input.value.trim()) {
             tasks.push(task = {
@@ -35,17 +39,17 @@
         input.value = "";
         input.focus();
     }
-
     const init = () => {
         const formElement = document.querySelector(".js-form");
+        
         formElement.addEventListener("submit", (e) => {
             e.preventDefault();
             onFormSubmit();
             setFocusClear();
         });
-        render(tasks);
+        
     }
-
+    render(tasks);
     init()
 
 
