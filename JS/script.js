6cfb5cc2e511838b6d7ac4439a350fc37332f8b4
Krzysztof Kwiecinski <kkwiecinski97@gmail.com>
2020-07-11 16:24:27 +0200

    changed css and added input focus onFormSubmit

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,7 @@
 {
     console.log("Good morning, Vietnam!");
     const tasksListElement = document.querySelector(".js-tasksList");
+    const input = (document.querySelector(".js-input"));
 
     const tasks = [
         task = {
@@ -32,33 +33,31 @@
         if (inputValue.value) {
             inputValue.value = ""
         }
-
     }
-
     const onFormSubmit = () => {
-        const input = (document.querySelector(".js-input"));
-
         if (input.value.trim()) {
             tasks.push(task = {
                 content: `${input.value}`
             });
-            tasksListElement.innerHTML += `<li class="todo__task"><button class="todo__button todo__button--check js-checkButton"></button><span class="todo__span">${task.content}</span><button class="todo__button todo__button--remove js-removeButton"></button></li>`;
-        } else {input.setAttribute("autofocus", "")}
-        console.log(tasks);
+            tasksListElement.innerHTML = "";
+            render();
+        } else {
+            // render()
+        };
     }
+    const setFocus = () => {
+        input.focus();
+    }
+
     const init = () => {
         const formElement = document.querySelector(".js-form");
         const submitButtonElement = document.querySelector(".js-buttonSubmit");
-
-
-
         formElement.addEventListener("submit", (e) => {
             e.preventDefault();
             onFormSubmit();
             clearInput();
-
-        })
-        submitButtonElement.addEventListener("click", onFormSubmit())
+            setFocus();
+        });
         render();
     }
 
