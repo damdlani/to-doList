c305e74ee3c6a8c283515c854706eab5dac0c790
Krzysztof Kwiecinski <kkwiecinski97@gmail.com>
2020-07-11 12:57:33 +0200

    added first JS and edited CSS

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,47 @@
-console.log("Good morning, Vietnam!");
+{
+    console.log("Good morning, Vietnam!");
+    
+
+    const onFormSubmit = () => {
+        console.log("Działam");;
+    }
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+
+        formElement.addEventListener("submit", (e) => {
+            e.preventDefault();
+            onFormSubmit();
+        })
+
+    }
+
+    init()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
