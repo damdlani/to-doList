89f3d04ac6c16a0d00efd37563400513674b2986
Krzysztof Kwiecinski <kkwiecinski97@gmail.com>
2020-07-11 14:56:51 +0200

    new task is added after submit

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,65 @@
 {
     console.log("Good morning, Vietnam!");
-    
+    const tasksListElement = document.querySelector(".js-tasksList");
+
+    const tasks = [
+        task = {
+            content: "test",
+            done: true,
+        },
+        task = {
+            content: "test",
+            done: true,
+        },
+        task = {
+            content: "test",
+            done: true,
+        },
+    ]
+    /*  funkcja do renderowania, odpalana za każdą zmianą
+        for each task of tasks dodaje do elementu todo__tasks element listy z buttonami 
+        //<li class="todo__task"><button>test</button><span>tekst</span><button>tekst</button></li>
+ */
+    const render = () => {
+
+        tasks.forEach((task, index) => {
+            tasksListElement.innerHTML += `<li class="todo__task"><button>test</button><span>${task.content}</span><button>tekst</button></li>`
+        });
+    }
+
+    const clearInput = () => {
+        let inputValue = (document.querySelector(".js-input"))
+        if (inputValue.value) {
+            inputValue.value = ""
+        }
+
+    }
 
     const onFormSubmit = () => {
-        console.log("Działam");;
+        const input = (document.querySelector(".js-input"));
+
+        if (input.value.trim()) {
+            tasks.push(task = {
+                content: `${input.value}`
+            });
+            tasksListElement.innerHTML += `<li class="todo__task"><button>test</button><span>${task.content}</span><button>tekst</button></li>`;
+        } else {input.setAttribute("autofocus", "")}
+        console.log(tasks);
     }
     const init = () => {
         const formElement = document.querySelector(".js-form");
+        const submitButtonElement = document.querySelector(".js-buttonSubmit");
+
+
 
         formElement.addEventListener("submit", (e) => {
             e.preventDefault();
             onFormSubmit();
-        })
+            clearInput();
 
+        })
+        submitButtonElement.addEventListener("click", onFormSubmit())
+        render();
     }
 
     init()
